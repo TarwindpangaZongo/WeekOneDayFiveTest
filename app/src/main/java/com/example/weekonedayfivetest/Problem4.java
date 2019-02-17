8d0221f7b6f2e3ef9616175d28063187cab68423
Felix Zongo <felix251@yahoo.fr>
2019-02-17 17:57:40 -0500

    problem4 corrected

--- app/src/main/java/com/example/weekonedayfivetest/Problem4.java
+++ app/src/main/java/com/example/weekonedayfivetest/Problem4.java
@@ -1,10 +1,15 @@
 package com.example.weekonedayfivetest;
 
 public class Problem4 {
-    public boolean checkAnagrams(String word1, String word2){
-        boolean check = true;
-        for (int i = 0,j = word2.length(); i < word1.length()/2; i++,j--){
-            if(word1.charAt(i) != word2.charAt(j)) check = false;
+    public static boolean checkAnagrams(String word1, String word2){
+        boolean check = false;
+        if (word1.length() == word2.length() ) {
+            for (int i = 0; i < word1.length(); i++ ) {
+                for (int j = 0; j <word2.length(); j++) {
+                    if(word1.charAt(i) != word2.charAt(j)) check = true;
+
+                }
+            }
         }
         return check;
     }
